8e5417553a6280b5d5b9829e501ec2e3a1c81b62
Emmanuel See Te <Twiistrz@users.noreply.github.com>
2017-03-11 20:57:48 +0800

    Update add.php

--- add.php
+++ add.php
@@ -21,7 +21,7 @@ if(isset($_POST['add'])) {
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
-	<title>Simple Add, Edit and View Data</title>
+	<title>Simple CRUD System</title>
 	<link rel="stylesheet" href="<?=HTTPHOST;?>assets/css/bootstrap.min.css">
 	<link rel="stylesheet" href="<?=HTTPHOST;?>assets/css/bootstrap.lumen.min.css">
 	<link rel="stylesheet" href="<?=HTTPHOST;?>assets/css/fontawesome.min.css">
@@ -30,7 +30,7 @@ if(isset($_POST['add'])) {
 
 	<div class="container">
 		<div class="page-header">
-			<h2 class="text-primary">Simple Add, Edit and View Data</h2>
+			<h2 class="text-primary">Simple CRUD System</h2>
 		</div>
 
 		<div class="row">
