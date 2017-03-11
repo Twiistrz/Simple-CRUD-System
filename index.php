ff59d2eac308035f468158de2be52f4338c13462
Emmanuel See Te <Twiistrz@users.noreply.github.com>
2017-03-11 20:55:29 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include_once 'db.config.php';
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
-	<title>Simple Add, Edit and View Data</title>
+	<title>Simple CRUD System</title>
 	<link rel="stylesheet" href="<?=HTTPHOST;?>assets/css/bootstrap.min.css">
 	<link rel="stylesheet" href="<?=HTTPHOST;?>assets/css/bootstrap.lumen.min.css">
 	<link rel="stylesheet" href="<?=HTTPHOST;?>assets/css/fontawesome.min.css">
